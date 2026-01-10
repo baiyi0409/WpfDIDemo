f1d45d7effb1eef16af28d4ac35b5daf2a367774
yao <2740574036@qq.com>
2026-01-10 16:16:53 +0800

    使用velopack实现自动更新，并测试

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,10 +9,9 @@ using System.ComponentModel;
 using WpfDIDemo.Services.ConfigurationLoader;
 using WpfDIDemo.Options;
 using Microsoft.Extensions.Options;
-using WpfDIDemo.Views;
 using System.Net.Security;
 using WpfDIDemo.ViewModels.AutoUpdate;
-using WpfDIDemo.Services.AutoUpdateService;
+using Velopack;
 
 namespace WpfDIDemo
 {
@@ -27,6 +26,15 @@ namespace WpfDIDemo
         //重写启动方法
         protected override void OnStartup(StartupEventArgs e)
         {
+            //Velopack自动更新
+            //逻辑为，将生成好的应用，进行打包
+            //生成命令为 dotnet publish WpfDIDemo.csproj -c Release --self-contained -r win-x64 -o .\publish   //根目录中多了publish
+            //打包命令为 vpk pack --packId WpfDIDemo --packVersion 1.0.0 --packDir .\publish --mainExe WpfDIDemo.exe //根目录中多了Releases
+            //其中 packDir .\publish 应根据实际发布的地址进行修改
+            //vpk pack --packId WpfDIDemo --packVersion 1.0.0 --packDir bin\Release\net6.0-windows\publish\win-x64 --mainExe WpfDIDemo.exe
+            //vpk pack --packId WpfDIDemo --packVersion 1.0.0 --packDir bin\Release\net6.0-windows\publish\win-x64
+            VelopackApp.Build().Run();
+
             //创建服务集合
             var services = new ServiceCollection();
 
@@ -45,9 +53,26 @@ namespace WpfDIDemo
             this.MainWindow = mainWindow;
             mainWindow.Show();
 
-            //自动更新
-            var autoUpdater = Services.GetRequiredService<AutoUpdateService>();
-            autoUpdater.Start("http://localhost:4555/AutoUpdate.json");
+            //自动更新模块
+            _ = Task.Run(async () =>
+            {
+                var mgr = new UpdateManager("http://localhost:4555/updates");
+                var newVersion = await mgr.CheckForUpdatesAsync();
+                if (newVersion == null) return;
+
+                //mgr.CurrentVersion
+
+                var result = MessageBox.Show(
+                    "发现新版本，是否立即更新？",
+                    "更新提示",
+                    MessageBoxButton.YesNo);
+
+                if (result != MessageBoxResult.Yes) return;
+
+                //其中有action<int> progress
+                await mgr.DownloadUpdatesAsync(newVersion);
+                mgr.ApplyUpdatesAndRestart(newVersion);
+            });
         }
 
         private void ConfigureServices(IServiceCollection services) 
@@ -60,18 +85,15 @@ namespace WpfDIDemo
             );
 
             //自动更新服务
-            services.AddSingleton<AutoUpdateService>();
 
             //本地化语言服务
             services.AddSingleton<ILocalizationService, LocalizationService>();
 
             //注册视图模型
             services.AddSingleton<IMainViewModel, MainViewModel>();
-            services.AddSingleton<IAutoUpdateViewModel, AutoUpdateViewModel>();
 
             //注册视图
             services.AddSingleton<MainWindow>();
-            services.AddSingleton<AutoUpdateView>();
         }
 
         //退出释放
--- ViewModels/Main/MainViewModel.cs
+++ ViewModels/Main/MainViewModel.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Velopack;
 using WpfDIDemo.Options;
 using WpfDIDemo.Services.LocalizationService;
 using WpfDIDemo.ViewModels.Main;
